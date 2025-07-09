03babb21d2fc9a697da4ebde2d234509df0bbe0f
0x002500 <tiger_pan@outlook.com>
2025-07-09 20:00:04 +0800

    [refactor] simplify calculateA function implementation

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,14 @@ pub struct Point {
 }
 
 fn calculateA(point1: Point, point2: Point, point3: Point) -> f64 {
-    (point1.y * (point2.x - point3.x)
-        + point2.y * (point3.x - point1.x)
-        + point3.y * (point1.x - point2.x))
-        / (point1.x * point1.x * (point2.x - point3.x)
-            + point2.x * point2.x * (point3.x - point1.x)
-            + point3.x * point3.x * (point1.x - point2.x))
-}
-
-fn calculateB(point1: Point, point2: Point, point3: Point) -> f64 {
-todo!()
+    let x1: f64 = point1.x;
+    let x2: f64 = point2.x;
+    let x3: f64 = point3.x;
+    let y1: f64 = point1.y;
+    let y2: f64 = point2.y;
+    let y3: f64 = point3.y;
+    (y1 * (x2 - x3) + y2 * (x3 - x1) + y3 * (x1 - x2))
+        / (x1 * x1 * (x2 - x3) + x2 * x2 * (x3 - x1) + x3 * x3 * (x1 - x2))
 }
 
 #[wasm_bindgen]
