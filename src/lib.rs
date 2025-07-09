5ddfae14b29b2270a03c3f1708a4feaaf5c04861
0x002500 <tiger_pan@outlook.com>
2025-07-09 20:04:20 +0800

    [feat] implement calculate_b function for quadratic calculation

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,17 @@ pub fn calculate_a(point1: Point, point2: Point, point3: Point) -> f64 {
         / (x1 * x1 * (x2 - x3) + x2 * x2 * (x3 - x1) + x3 * x3 * (x1 - x2))
 }
 
-
+#[wasm_bindgen]
+pub fn calculate_b(point1: Point, point2: Point, point3: Point) -> f64 {
+    let x1: f64 = point1.x;
+    let x2: f64 = point2.x;
+    let x3: f64 = point3.x;
+    let y1: f64 = point1.y;
+    let y2: f64 = point2.y;
+    let y3: f64 = point3.y;
+    let a = calculate_a(point1, point2, point3);
+    (y1-y2)/(x1-x2)-a*(x1+x2)
+}
 
 #[wasm_bindgen]
 pub fn point2quadraticfunction(point1: Point, point2: Point, point3: Point) {}
