5325572d3fd87ebadfb35dcaed89bfb75d3b1c2c
0x002500 <tiger_pan@outlook.com>
2025-07-09 20:00:35 +0800

    [feat] expose calculateA function to wasm

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,8 @@ pub struct Point {
     pub y: f64,
 }
 
-fn calculateA(point1: Point, point2: Point, point3: Point) -> f64 {
+#[wasm_bindgen]
+pub fn calculateA(point1: Point, point2: Point, point3: Point) -> f64 {
     let x1: f64 = point1.x;
     let x2: f64 = point2.x;
     let x3: f64 = point3.x;
@@ -16,6 +17,8 @@ fn calculateA(point1: Point, point2: Point, point3: Point) -> f64 {
     (y1 * (x2 - x3) + y2 * (x3 - x1) + y3 * (x1 - x2))
         / (x1 * x1 * (x2 - x3) + x2 * x2 * (x3 - x1) + x3 * x3 * (x1 - x2))
 }
+
+
 
 #[wasm_bindgen]
 pub fn point2quadraticfunction(point1: Point, point2: Point, point3: Point) {}
