a50b43f9444647c14c0eb34ac066158b69d265c0
0x002500 <tiger_pan@outlook.com>
2025-07-09 20:16:18 +0800

    [fix] specify type for variable 'a' in calculate_b function

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,7 @@ pub fn calculate_b(point1: Point, point2: Point, point3: Point) -> f64 {
     let x2: f64 = point2.x;
     let y1: f64 = point1.y;
     let y2: f64 = point2.y;
-    let a = calculate_a(point1, point2, point3);
+    let a: f64 = calculate_a(point1, point2, point3);
     (y1 - y2) / (x1 - x2) - a * (x1 + x2)
 }
 
