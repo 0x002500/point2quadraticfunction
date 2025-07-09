f32221fbc96cc112364081329363ae1733caed1a
0x002500 <tiger_pan@outlook.com>
2025-07-09 20:11:13 +0800

    [fix] format calculate_b function

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@ pub fn calculate_b(point1: Point, point2: Point, point3: Point) -> f64 {
     let y1: f64 = point1.y;
     let y2: f64 = point2.y;
     let a = calculate_a(point1, point2, point3);
-    (y1-y2)/(x1-x2)-a*(x1+x2)
+    (y1 - y2) / (x1 - x2) - a * (x1 + x2)
 }
 
 #[wasm_bindgen]
